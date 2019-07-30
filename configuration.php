ef7d47a8dd6227da5e02d8bbb998cdb1ad13a041
LarsMog <lars@webitall.dk>
2019-07-30 14:59:13 +0200

    Joomla com_api

--- configuration.php
+++ configuration.php
@@ -1,7 +1,7 @@
 <?php
 class JConfig {
 	public $offline = false;
-	public $offline_message = 'This site is down for maintenance.<br>Please check back again soon.';
+	public $offline_message = 'This site is down for maintenance.<br />Please check back again soon.';
 	public $display_offline_message = 1;
 	public $offline_image = '';
 	public $sitename = 'rafus NET';
@@ -11,7 +11,7 @@ class JConfig {
 	public $access = 1;
 	public $debug = false;
 	public $debug_lang = false;
-	public $debug_lang_const = true;
+	public $debug_lang_const = 1;
 	public $dbtype = 'mysqli';
 	public $host = 'mysql2.gigahost.dk';
 	public $user = 'rafus';
@@ -28,7 +28,7 @@ class JConfig {
 	public $ftp_user = '';
 	public $ftp_pass = '';
 	public $ftp_root = '';
-	public $ftp_enable = 0;
+	public $ftp_enable = false;
 	public $offset = 'UTC';
 	public $mailonline = true;
 	public $mailer = 'mail';
@@ -48,9 +48,9 @@ class JConfig {
 	public $MetaDesc = '';
 	public $MetaKeys = '';
 	public $MetaTitle = true;
-	public $MetaAuthor = true;
+	public $MetaAuthor = false;
 	public $MetaVersion = false;
-	public $robots = '';
+	public $robots = 'noindex, nofollow';
 	public $sef = true;
 	public $sef_rewrite = false;
 	public $sef_suffix = false;
@@ -63,4 +63,36 @@ class JConfig {
 	public $session_handler = 'database';
 	public $shared_session = false;
 	public $session_metadata = true;
+	public $memcached_persist = true;
+	public $memcached_compress = false;
+	public $memcached_server_host = 'localhost';
+	public $memcached_server_port = 11211;
+	public $redis_persist = true;
+	public $redis_server_host = 'localhost';
+	public $redis_server_port = 6379;
+	public $redis_server_auth = '';
+	public $redis_server_db = 0;
+	public $proxy_enable = false;
+	public $proxy_host = '';
+	public $proxy_port = '';
+	public $proxy_user = '';
+	public $proxy_pass = '';
+	public $massmailoff = false;
+	public $replyto = '';
+	public $replytoname = '';
+	public $MetaRights = '';
+	public $sitename_pagetitles = 0;
+	public $force_ssl = 0;
+	public $session_filesystem_path = '';
+	public $session_memcached_server_host = 'localhost';
+	public $session_memcached_server_port = 11211;
+	public $session_redis_persist = 1;
+	public $session_redis_server_host = 'localhost';
+	public $session_redis_server_port = 6379;
+	public $session_redis_server_auth = '';
+	public $session_redis_server_db = 0;
+	public $frontediting = 1;
+	public $cookie_domain = '';
+	public $cookie_path = '';
+	public $asset_id = 1;
 }
